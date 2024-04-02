0768ec2bb030f319f19fa2706dcfe1b90417ee15
Lakshan Jayaweera <127861567+LakshanJayaweera426@users.noreply.github.com>
2024-04-02 13:18:58 +0530

    Develop products details html pages

--- environment/project/application/urls.py
+++ environment/project/application/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path("",views.home)
-]
+    path("",views.home),
+    path("category/<slug:val>", views.CategoryView.as_view(),name="category"),
+    path("category-title/<val>", views.CategoryTitle.as_view(),name="category-title"),
+    path("product-detail/<int:pk>", views.ProductDetails.as_view(),name="product-detail"),
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
