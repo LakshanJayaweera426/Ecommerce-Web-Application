007a418c009f7d8be681bb0df5748a2b99d36379
Lakshan Jayaweera <127861567+LakshanJayaweera426@users.noreply.github.com>
2024-04-04 15:32:01 +0530

    Create Registration and Login page successfully

--- environment/project/application/views.py
+++ environment/project/application/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.views import View
 from . models import Product
 from django.db.models import Count
+from . forms import CustomerRegistrationForm
+from django.contrib import messages
+
 
 def home(request):
     return render(request, "app/home.html")
@@ -28,3 +31,27 @@ class ProductDetails(View):
     def get(self,request,pk):
         product = Product.objects.get(pk=pk)
         return render(request, "app/productdetail.html",locals())
+    
+
+class CustomerRegistrationView(View):
+    def get(self,request):
+        form = CustomerRegistrationForm()
+        return render(request, 'app/customerregistration.html', locals())
+    def post(self,request):
+        form = CustomerRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Congratulations! User Register Successfully")
+        else:
+            messages.warning(request, "Invalid Input Data")
+        return render(request, 'app/customerregistration.html', locals())  
+    
+
+class ProfileView(View):
+    def get(self,request):
+        return render(request, 'app/profile.html', locals())
+    def post(self,request):
+        return render(request, 'app/profile.html', locals())
+
+
+  
