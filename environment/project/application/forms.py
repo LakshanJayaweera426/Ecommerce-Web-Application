b1d18f05e014dc27c3c4a3aac1220315f386ce42
Lakshan Jayaweera <127861567+LakshanJayaweera426@users.noreply.github.com>
2024-04-06 12:24:38 +0530

    Developed change password web pages.....

--- environment/project/application/forms.py
+++ environment/project/application/forms.py
@@ -8,7 +8,6 @@ class LoginForm(AuthenticationForm):
     username = UsernameField(widget=forms.TextInput(attrs={'autofocus':'True','class':'form-control'}))
     password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control'}))
 
-
 class CustomerRegistrationForm(UserCreationForm):
     username = forms.CharField(widget=forms.TextInput(attrs={'autofocus':'True','class':'form-control'}))
     email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control'}))
@@ -18,6 +17,13 @@ class CustomerRegistrationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username','email','password1','password2']
+
+class MyPasswordChangeForm(PasswordChangeForm):
+    old_password = forms.CharField(label = 'Old Password', widget=forms.PasswordInput(attrs={'autofocus':'True','autocomplete':'current-password','class':'form-control'}))
+    new_password1 = forms.CharField(label = 'New Password', widget=forms.PasswordInput(attrs={'autocomplete':'current-password','class':'form-control'}))
+    new_password2 = forms.CharField(label = 'Confirm Password', widget=forms.PasswordInput(attrs={'autocomplete':'current-password','class':'form-control'}))
+
+
 
 class  MyPasswordResetForm(PasswordChangeForm):
     pass
