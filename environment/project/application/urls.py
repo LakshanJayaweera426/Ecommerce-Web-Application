c0d55de21da1aa2f1921ee2ad8fcc9547979cca8
Lakshan Jayaweera <127861567+LakshanJayaweera426@users.noreply.github.com>
2024-04-09 15:40:40 +0530

    xyz

--- environment/project/application/urls.py
+++ environment/project/application/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib import admin
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
@@ -49,3 +50,7 @@ urlpatterns = [
 
 
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+admin.site.site_header = "Unify Dairy"
+admin.site.site_title = "Unify Dairy"
+admin.site.site_index_title = "Welcome to Unify Dairy Shop"
